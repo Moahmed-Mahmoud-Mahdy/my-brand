9300961c71c3228ca82ad442f9287573c644b1e4
Mahdy <145232016+Moahmed-Mahmoud-Mahdy@users.noreply.github.com>
2026-06-25 20:46:25 +0300

    feat: integrate Turso (libSQL) online database via Prisma adapter

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,27 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+let prismaInstance: PrismaClient
+
+const url = process.env.TURSO_DATABASE_URL
+const token = process.env.TURSO_AUTH_TOKEN
+
+if (url && url !== 'undefined' && token && token !== 'undefined') {
+  const adapter = new PrismaLibSql({
+    url: url,
+    authToken: token,
+  })
+  prismaInstance = new PrismaClient({ adapter, log: ['query'] })
+} else {
+  prismaInstance = new PrismaClient({
     log: ['query'],
   })
+}
+
+export const db = globalForPrisma.prisma ?? prismaInstance
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
